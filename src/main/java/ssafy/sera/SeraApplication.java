48754f82c97fcd278d5283686ac528aa2c733acd
최성철 <tkdlqjf@gmail.com>
2024-10-21 09:42:51 +0900

    start: 프로젝트 시작

---
+++ src/main/java/ssafy/sera/SeraApplication.java
@@ -0,0 +1,13 @@
+package ssafy.sera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeraApplication.class, args);
+    }
+
+}
