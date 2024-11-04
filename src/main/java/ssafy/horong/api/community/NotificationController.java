84948db7dd19dd741c06f565672d06dc5435965b
최성철 <tkdlqjf@gmail.com>
2024-11-04 11:02:34 +0900

    fix : 알람 로직 api 버그 수정

--- src/main/java/ssafy/horong/api/community/NotificationController.java
+++ src/main/java/ssafy/horong/api/community/NotificationController.java
@@ -22,12 +22,4 @@ public class NotificationController {
         notificationService.markAsRead(notificationId, type);
         return CommonResponse.ok("알림이 읽음 처리되었습니다.", null);
     }
-
-    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
-    @Operation(summary = "알림 전송", description = "알림을 전송합니다.")
-    @PostMapping("/send")
-    public CommonResponse<Void> sendNotification(@RequestParam String message, @RequestParam Long userId) {
-        notificationService.sendNotificationToUser(message, userId);
-        return CommonResponse.ok("알림이 전송되었습니다.", null);
-    }
 }
