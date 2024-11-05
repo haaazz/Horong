4a93110f28a77d1ff27528ffbd2f1ca50db5a136
최성철 <tkdlqjf@gmail.com>
2024-11-05 14:43:17 +0900

    feat : 알람 연결 로직 추가

--- src/main/java/ssafy/horong/api/community/NotificationController.java
+++ src/main/java/ssafy/horong/api/community/NotificationController.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import ssafy.horong.api.CommonResponse;
+import ssafy.horong.common.util.NotificationUtil;
 import ssafy.horong.domain.community.entity.Notification;
 import ssafy.horong.domain.community.service.NotificationService;
 
@@ -14,6 +16,7 @@ import ssafy.horong.domain.community.service.NotificationService;
 public class NotificationController {
 
     private final NotificationService notificationService;
+    private final NotificationUtil notificationUtil;
 
     @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @Operation(summary = "알림 읽음 처리", description = "알림을 읽음 처리합니다.")
@@ -21,5 +24,19 @@ public class NotificationController {
     public CommonResponse<Void> markAsRead(@PathVariable Long notificationId, @RequestParam Notification.NotificationType type) {
         notificationService.markAsRead(notificationId, type);
         return CommonResponse.ok("알림이 읽음 처리되었습니다.", null);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @Operation(summary = "알림 스트림", description = "알림을 스트림으로 전송합다.")
+    @GetMapping("/notifications/stream")
+    public SseEmitter streamNotifications() {
+        SseEmitter emitter = new SseEmitter(600000L); // 60초 동안 연결 유지
+        notificationUtil.addEmitter(emitter);
+
+        emitter.onCompletion(() -> notificationUtil.removeEmitter(emitter));
+        emitter.onTimeout(() -> notificationUtil.removeEmitter(emitter));
+        emitter.onError((e) -> notificationUtil.removeEmitter(emitter));
+
+        return emitter;
     }
 }
