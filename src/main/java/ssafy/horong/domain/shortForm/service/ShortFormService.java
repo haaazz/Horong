8f15a916ec58a090416639db1cfe4c9511a00c45
최성철 <tkdlqjf@gmail.com>
2024-11-11 09:57:51 +0900

    feat : 숏폼 데이터 파트와 연결 코드 초안 추가

--- src/main/java/ssafy/horong/domain/shortForm/service/ShortFormService.java
+++ src/main/java/ssafy/horong/domain/shortForm/service/ShortFormService.java
@@ -1,4 +1,9 @@
 package ssafy.horong.domain.shortForm.service;
 
+import ssafy.horong.api.shortForm.response.ShortFromListResponse;
+
+import java.util.List;
+
 public interface ShortFormService{
+    List<ShortFromListResponse> getShortFormList();
 }
