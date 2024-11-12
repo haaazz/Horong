eaecc38ebfd1d6eb0b975dc92e389ce4efe35667
최성철 <tkdlqjf@gmail.com>
2024-11-12 14:58:43 +0900

    fix : 한국어학습 유저가 한국어 설정시 발생하는 오류 수정

---
+++ src/main/java/ssafy/horong/api/education/response/TodayTranslatedWordResponse.java
@@ -0,0 +1,13 @@
+package ssafy.horong.api.education.response;
+
+import ssafy.horong.domain.member.common.Language;
+
+public record TodayTranslatedWordResponse(
+        Long id,
+        Long educationId, // 연관된 Education 엔티티의 ID만 포함
+        Language language,
+        String transWord,
+        String transDefinition,
+        String transExample1,
+        String transExample2
+) {}
