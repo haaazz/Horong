f08bb51682e6a5c2917213fc5125ec5e198487a3
최성철 <tkdlqjf@gmail.com>
2024-11-14 14:08:03 +0900

    fix : 한국어 학습 기록 조회 시 발음 설명 추가

--- src/main/java/ssafy/horong/api/education/response/GetEducationRecordByWordResponse.java
+++ src/main/java/ssafy/horong/api/education/response/GetEducationRecordByWordResponse.java
@@ -5,6 +5,8 @@ import java.util.List;
 public record GetEducationRecordByWordResponse(
         String word,
         Long wordId,
+        String pronunciation,
+        String definition,
         List<EducationRecordResponse> educationRecordList
 ) {
 }
